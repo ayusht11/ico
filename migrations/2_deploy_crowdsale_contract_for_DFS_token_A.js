ba76dc7caa7d97e55a9c78f4a90d7e2574328567
Ayush Tiwari <ayusht11@outlook.com>
2020-07-06 21:31:47 +0530

    Re Deployed Contracts

--- migrations/2_deploy_crowdsale_contract_for_DFS_token_A.js
+++ migrations/2_deploy_crowdsale_contract_for_DFS_token_A.js
@@ -1,23 +1,23 @@
 const DFSTokenACrowdsale = artifacts.require("./DFSTokenACrowdsale.sol");
 
-const _startTime = 1509017400;
-const _endTime = 1509087400;
+const _startTime = 1519017400;
+const _endTime = 1519087400;
 const _initialRate = 100;
 const _goal = "10000000000000000000";
 const _initialCap = "3000000000000000000";
 const _wallet = "0xc22729e46412e535275b8cc2561520a1abb8ea1c";
 const _milestones = [
-  1509017400, 100, "3000000000000000000",//presale start time, rate and weiCap
-  1509027400, 0, "3000000000000000000",  //presale end time, rate (must be to be 0) and weiCap
-  1509037400, 80, "3000000000000000000", //milestone 1 (sale start)
-  1509047400, 70, "3000000000000000000", //milestone 2
-  1509057400, 60, "3000000000000000000", //milestone 3
-  1509067400, 50, "3000000000000000000", //milestone 4
-  1509077400, 40, "3000000000000000000", //milestone 5
-  1509087400, 0, 0                       //sale end- price and weiCap must be 0 
+  1519017400, 100, "3000000000000000000",//presale start time, rate and weiCap
+  1519027400, 0, "3000000000000000000",  //presale end time, rate (must be to be 0) and weiCap
+  1519037400, 80, "3000000000000000000", //milestone 1 (sale start)
+  1519047400, 70, "3000000000000000000", //milestone 2
+  1519057400, 60, "3000000000000000000", //milestone 3
+  1519067400, 50, "3000000000000000000", //milestone 4
+  1519077400, 40, "3000000000000000000", //milestone 5
+  1519087400, 0, 0                       //sale end- price and weiCap must be 0 
 ];
 
-module.exports = function(deployer) {
+module.exports = (deployer) => {
   deployer.deploy(DFSTokenACrowdsale, _startTime, _endTime, _initialRate, _goal, _initialCap, _wallet,
-    _milestones, { from: "0xecd0dbd7ac6e376945d3c3a02035e8a2448ecb06" });
+    _milestones, { from: "0xf7bd6f262b83da35d74137bde6a317b119275b32" });
 };
